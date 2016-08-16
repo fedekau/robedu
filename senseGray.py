d3a89087b79ce1fbf370f8a8a05a1a19230a1275
Federico Kauffman <fedekau@gmail.com>
2016-08-15 21:31:22 -0300

    Add basic line follower

---
+++ senseGray.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.insert(0,'/usr/share/sugar/activities/TurtleBots.activity/plugins/butia')
+
+
+from pybot import usb4butia
+
+robot =  usb4butia.USB4Butia()
+version = robot.getFirmwareVersion()
+
+while True:
+	sensor1 = robot.getGray(5)
+	sensor2 = robot.getGray(6)
+	print str(sensor1) + " --- " + str(sensor2)
