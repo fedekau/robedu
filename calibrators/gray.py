ff7ec922fcbef8ba8462f737b89e1bfb1b4820e2
Federico Kauffman <federico@wyeworks.com>
2016-08-20 15:03:59 -0300

    Load calibration if it exists

--- calibrators/gray.py
+++ calibrators/gray.py
@@ -1,10 +1,31 @@
+import os
+
 class GrayCalibrator:
+    CONFIG_FILE = "gray_sensors.conf"
     MEASUREMENTS = 1000
 
     def __init__(self, sensors):
 	self.sensors = sensors
 
     def calibrate(self):
+	if (is_calibrated()):
+	    load_calibration()
+	else:
+	    generate_calibration()
+
+    def is_calibrated(self):
+	return os.path.exists(CONFIG_FILE)
+
+    def load_calibration(self):
+	with open(CONFIG_FILE, 'a+') as f:
+		config = f.readLines()
+		for i in config:
+		    p = i.split('\t')[0]
+		    t = i.split('\t')[1]
+		    sensor = next(s for s in sensor if s.port == p)
+		    sensor.threshold = t
+
+    def generate_calibration(self):
 	avg = []
 	colors = ['white', 'black']
 	for c in colors:
@@ -21,6 +42,10 @@ class GrayCalibrator:
 	    threshold = min(avg[0][i], avg[1][i]) + (abs(avg[0][i] - avg[1][i]) / 2)
 	    self.sensor.threshold = threshold
 	    print "The threshold for sensor in %(sensor.port)s is: %(threshold)s" % locals()
+	with open(CONFIG_FILE, "w") as f:
+	    for s in self.sensors:
+		f.write(str(s.port) + '\t' + str(s.threshold) + '\n')
+
 
     def measure_average(self):
 	s = [0] * len(self.sensors)
